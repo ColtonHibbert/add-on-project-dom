294e11364e222765877681645d2d64712eadb00d
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-04-20 13:12:42 -0600

    working through forms api

--- bom.js
+++ bom.js
@@ -105,9 +105,45 @@
                 }
             }
         }
+    Forms
+        Constraint Validation DOM methods
+            Property	        Description
+            checkValidity()	    Returns true if an input element contains valid data
+            setCustomValidity()	Sets the validationMessage property of an input element
 
-
-        
-
-
+            checkValidity() example
+                function myFunction() {
+                    var inpObj = document.getElementById("id1");
+                    if (!inpObj.checkValidity()) {
+                        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+                    }
+                }
+        Constraint Validation DOM Properties
+            Property	        Description
+            validity	        Contains boolean properties related to the validity of an input element.
+            validationMessage	Contains the message a browser will display when the validity is false.
+            willValidate	    Indicates if an input element will be validated
+        Validity Properties of input elements
+            Property	    Description
+            customError	    Set to true, if a custom validity message is set.
+            patternMismatch	Set to true, if an element's value does not match its pattern attribute.
+            rangeOverflow	Set to true, if an element's value is greater than its max attribute.
+            rangeUnderflow	Set to true, if an element's value is less than its min attribute.
+            stepMismatch	Set to true, if an element's value is invalid per its step attribute.
+            tooLong	        Set to true, if an element's value exceeds its maxLength attribute.
+            typeMismatch	Set to true, if an element's value is invalid per its type attribute.
+            valueMissing	Set to true, if an element (with a required attribute) has no value.
+            valid	        Set to true, if an element's value is valid.
+                
+            example rangeOverflow property
+                <input id="id1" type="number" max="100">
+                <button onclick="myFunction()">OK</button>   
+                   
+                function myFunction() {
+                    var txt = "";
+                    if (document.getElementById("id1").validity.rangeOverflow) {
+                        txt = "Value too large";
+                    }
+                    document.getElementById("demo").innerHTML = txt;
+                }
 */
